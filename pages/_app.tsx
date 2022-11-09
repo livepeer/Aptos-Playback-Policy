af7b8f72dfe674ce8539663d18960b679a2426a7
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-11-09 16:16:26 -0600

    configure and connect providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
+import {useMemo, createContext} from 'react'
 import type { AppProps } from 'next/app'
+import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react'
+import { AptosClient } from 'aptos'
+
+// Setting up Aptos client
+export const AptosContext = createContext<AptosClient | null>( null );
+const aptosClient = useMemo (() => new AptosClient('https://fullnode.devnet.aptoslabs.com/v1'), []))
+
+// Setting up Livepeer Studio client
+const client = createReactClient( {
+  provider: studioProvider( {
+    apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY
+  })
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AptosContext.Provider value={aptosClient}>
+      <LivepeerConfig client={ client }>
+      < Component {...pageProps } />
+      </LivepeerConfig>
+    </AptosContext.Provider>
+    )
 }
