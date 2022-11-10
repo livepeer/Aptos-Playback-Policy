d2d24c1f4f59dfc090b67f65b02f0e5311043711
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-11-10 12:53:10 -0600

    create api to generate JWT

---
+++ pages/api/createJWT.ts
@@ -0,0 +1,56 @@
+import {signAccessJwt} from 'livepeer/crypto'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+
+// Set type for payload information to create JWT
+export type CreateSignedPlaybackBody = {
+  playbackId: string;
+  secret: string;
+  address: string;
+}
+
+// Set type for revieving JWT
+export type CreateSignedPlaybackResponse = {
+  token: string;
+}
+
+// Set variable for access control signing keys
+const accessControlPrivateKey = process.env.PRIVATE_KEY;
+const accessControlPublicKey = process.env.PUBLIC_KEY;
+
+const handler = async ( req: NextApiRequest, res: NextApiResponse ) => {
+  try {
+    const method = req.method;
+
+    if ( method === 'POST' ) {
+      if ( !accessControlPrivateKey || !accessControlPublicKey ) {
+        return res.status( 500 ).json( { message: 'No private/public key configured.' } );
+      }
+      const { playbackId, secret, address }: CreateSignedPlaybackBody = req.body;
+      if ( !playbackId || !secret ) {
+        return res.status( 400 ).json( { message: 'Missing data in body.' } );
+      }
+      if ( secret !== 'supersecretkey' ) {
+        return res.status(401).json({message: 'Incorrect secret.'})
+      }
+      const token = await signAccessJwt( {
+        privateKey: accessControlPrivateKey,
+        publicKey: accessControlPublicKey,
+        issuer: 'Aptos',
+        playbackId,
+        expiration: '1hr',
+        custom: {
+          address
+        }
+      } )
+      return res.status(200).json({token})
+    }
+    res.setHeader( 'Allow', [ 'POST' ] );
+    return res.status( 405 ).end( `Method ${method} Not Allowed` );
+  } catch (error) {
+    console.error( error );
+    res.status( 500 ).json( { message: ( error as Error )?.message ?? 'Error' } );
+    
+  }
+}
+export default handler;
