d0bba3168cb9097069aa818a232a813ab9610776
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-11-11 11:37:34 -0600

    update clients

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,30 @@
-import '../styles/globals.css'
-import {useMemo, createContext} from 'react'
-import type { AppProps } from 'next/app'
-import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react'
-import { AptosClient } from 'aptos'
+import '../styles/globals.css';
+import { useMemo, createContext } from 'react';
+import type { AppProps } from 'next/app';
+import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react';
+import { AptosClient } from 'aptos';
 
 // Setting up Aptos client
-export const AptosContext = createContext<AptosClient | null>( null );
+export const AptosContext = createContext<AptosClient | null>(null);
 
 // Setting up Livepeer Studio client
-const client = createReactClient( {
-  provider: studioProvider( {
-    apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY
-  })
-})
-
+const client = createReactClient({
+  provider: studioProvider({
+    apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY,
+  }),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  const aptosClient = useMemo( () => new AptosClient( 'https://fullnode.devnet.aptoslabs.com/v1' ), [] );
-  
+  const aptosClient = useMemo(
+    () => new AptosClient('https://fullnode.devnet.aptoslabs.com/v1'),
+    []
+  );
+
   return (
-    <AptosContext.Provider value={aptosClient}>
-      <LivepeerConfig client={ client }>
-      < Component {...pageProps } />
-      </LivepeerConfig>
-    </AptosContext.Provider>
-    )
+    <LivepeerConfig client={client}>
+      <AptosContext.Provider value={aptosClient}>
+        <Component {...pageProps} />
+      </AptosContext.Provider>
+    </LivepeerConfig>
+  );
 }
