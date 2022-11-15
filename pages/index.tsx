9adfe8ad92d12756d345ccd968acff2236105654
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-11-15 13:19:15 -0600

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import aptosImage from '../public/Aptos.jpeg'
 import {useState} from 'react'
 import styles from '../styles/Home.module.css';
 import 'tailwindcss/tailwind.css';
+import Wallet from '../components/Wallet';
+import CreateGatedStream from '../components/CreateGatedStream';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 export default function Home() {
   
+  const queryClient = new QueryClient();
+  const [walletAddress, setWalletAddress] = useState();
 
   return (
     <div className={styles.container}>
@@ -24,16 +28,12 @@ export default function Home() {
           Welcome to <span className='text-aptos-green'>Aptos</span> Playback Policy
         </h1>
 
-        <Link href='/gateStream'>
-          <div className='rounded outline outline-offset-2 outline-1 outline-aptos-green p-4 m-4 text-xl bg-slate-800 hover:outline-slate-800 text-aptos-green hover:text-gray-100 cursor-pointer'>
-            Click to Gate Stream
-          </div>
-        </Link>
-        <Link href='/viewStream'>
-          <div className='rounded outline outline-offset-2 outline-1 outline-aptos-green p-4 m-4 text-xl bg-slate-800 hover:outline-slate-800 text-aptos-green hover:text-gray-100 cursor-pointer'>
-            Click to View Stream
-          </div>
-        </Link>
+        <Wallet setWalletAddress={setWalletAddress} />
+        {walletAddress ? (
+          <QueryClientProvider client={queryClient}>
+            <CreateGatedStream walletAddress={walletAddress} />
+          </QueryClientProvider>
+        ) : null}
       </main>
     </div>
   );
