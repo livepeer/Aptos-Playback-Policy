e5c7937c6e5744cb50b0035ff81b9f78b30b16f4
Shih-Yu Hwang <shihyuhwang@gmail.com>
2022-11-15 13:18:23 -0600

    create ViewStream component

--- components/ViewGatedStream.tsx
+++ components/ViewGatedStream.tsx
@@ -1,59 +1,23 @@
-import { useMemo, useState } from 'react'
-import { Player, useStream } from '@livepeer/react';
-import { useMutation } from '@tanstack/react-query';
-import { SignMesagePayload, SignMessageResponse } from '../pages/api/signMessage';
+import { Player } from '@livepeer/react';
 
 interface CreatedStreamProps {
   playbackId: string;
   jwt: string;
+  streamName: string;
 }
 
-export default function ViewGatedStream(
-  props: CreatedStreamProps,
-  { walletAddress }: { walletAddress: string } ) {
-  
-  
-  const { mutate: signMessage, data: signMessageResponse } = useMutation( {
-    mutationFn: async () => {
-      if ( !walletAddress ) {
-        throw new Error( 'No address' );
-      }
-  
-      const body: SignMesagePayload = {
-        address: true,
-        application: true,
-        chainId: true,
-        message: 'Access allowed for stream',
-        nonce: ''
-      }
+export default function ViewGatedStream(props: CreatedStreamProps ) {
 
-      const response = await fetch( '/api/signMessage', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( body ),
-      } );
-  
-      return response.json() as Promise<SignMessageResponse>;
-    },
-  
-    });
-  
-  console.log(walletAddress);
-  
   return (
     <>
-      {/* {props.playbackId ? ( */}
-        <div className='w-1/2'>
-          <Player
-            playbackId={props.playbackId}
-            jwt={ props.jwt}
-            muted
-            autoPlay
-            showPipButton
-          />
-        </div>
-      {/* ) :  */}
-      {/* null} */}
+      <div>
+        <Player
+          title={props.streamName}
+          playbackId={props.playbackId}
+          showPipButton
+          jwt={props.jwt}
+        />
+      </div>
     </>
   );
 }
